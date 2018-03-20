05f3ead57f1ace78de3107f42faae0ec00345dea
dimmaski <diogo.fernandes.12@hotmail.com>
2018-03-20 17:42:58 +0000

    arc nas canvas

--- history/history.js
+++ history/history.js
@@ -10,18 +10,14 @@ function main() {
 
   var c = document.getElementById("historyCanvas");
   var ctx = c.getContext("2d");
-  var background = new Image();
-  background.src = "../resources/background_clean.jpg";
-
-  background.onload = function(){
-      ctx.drawImage(background,0,0);
-  }
 
 
-
-
+  ctx.strokeStyle = "White";
   ctx.beginPath();
-  ctx.arc(100, 75, 100, 0, 2 * Math.PI);
+
+  ctx.arc(230, 410, 300, 0, 2 * Math.PI);
+  ctx.arc(205, 430, 400, 0, 2 * Math.PI);
+  ctx.arc(100, 300, 50, 0, 2 * Math.PI);
   ctx.stroke();
 
 }
