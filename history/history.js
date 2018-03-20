e08bc46b0b6f55a496bba88b672018d90fff647d
dimmaski <diogo.fernandes.12@hotmail.com>
2018-03-20 17:30:43 +0000

    tentativ de corrigir fundo canvas

--- history/history.js
+++ history/history.js
@@ -5,12 +5,23 @@
 	window.addEventListener("load", main);
 }());
 
+
 function main() {
 
   var c = document.getElementById("historyCanvas");
   var ctx = c.getContext("2d");
+  var background = new Image();
+  background.src = "../resources/background_clean.jpg";
+
+  background.onload = function(){
+      ctx.drawImage(background,0,0);
+  }
+
+
+
+
   ctx.beginPath();
-  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.arc(100, 75, 100, 0, 2 * Math.PI);
   ctx.stroke();
 
 }
