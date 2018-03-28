1791b2b94be48186ab9e2e9012f3b0e0039b993b
tpessoa <tiagopessoa123@hotmail.com>
2018-03-29 00:33:37 +0100

    shoot

--- play/play.js
+++ play/play.js
@@ -9,153 +9,118 @@ var LEFT = false;
 var RIGHT = false;
 var UP = false;
 var DOWN = false;
+var SPACE = false;
+
+// qt de balas
+var SIZE_POOL = 10;
+var countBullets = 0;
+
+// repositorios para desenhar imagens com sprites
+var imageRepository = new function() {
+	var nLoad = 0;
+	var totLoad = 3;
+
+	this.bullet = new Image();
+	this.background = new Image();
+	this.ship = new Image(); 
+
+	function imageLoad() {
+		nLoad++;
+		if (nLoad == totLoad) {
+			// meter condição aqui para disparar o init na main
+		}
+	}
+
+	this.background.onload = function() {
+		imageLoad();
+	}
+	this.ship.onload = function() {
+		imageLoad();
+	}
+	this.bullet.onload = function() {
+		imageLoad();
+	}
+
+	this.background.id = "background";
+	this.ship.id = "ship1";
+	this.bullet.id = "bullet";
+		
+	this.background.src = "../resources/backgrounds/background_space2.jpg";
+	this.ship.src = "../resources/ships/ship1.png"; 
+	this.bullet.src = "../resources/bullets/bullet1.png";
+}
 
 
 function main()
 {
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
-	var spArray;  //sprite array
-
-	canvas.addEventListener("initend", initEndHandler);
 	init(ctx);  //carregar todos os componentes
-
-	//funções locais para gestão de eventos
-	function initEndHandler(ev)
-	{
-		//instalar listeners do rato	
-		ctx.canvas.addEventListener("click", cch);
-		
-		spArray = ev.spArray;
-
-		//iniciar a animação
-		startAnim(ctx, spArray);
-		
-	}
-
-	var cch = function(ev)
-	{
-		canvasClickHandler(ev, ctx, spArray);	
-	}
-
-	
 }
 
 
 function init(ctx) {
 	var nLoad = 0;
-	var totLoad = 1;
+	var totLoad = 2;
 	var spArray = new Array(totLoad);
+	var bulletsArray = [];
 
-	var background = new Image();
-	background.addEventListener("load", bgLoader);
-	background.id= "background";
-	background.src = "../resources/backgrounds/background_space2.jpg";
+	// carregar sprites
+	var cw = ctx.canvas.width;
+	var ch = ctx.canvas.height;
 
-	var img = new Image(); 
-	img.addEventListener("load", shipsLoader);
-	img.id= "ship1";
-	img.src = "../resources/ships/ship1.png"; 
-	//dá ordem de carregamento da imagem
+	var nw = imageRepository.background.naturalWidth;
+	var nh = imageRepository.background.naturalHeight;
+	var sp = new SpriteImage(0, 0, nw, nh, 3, false, imageRepository.background);
+	spArray[nLoad] = sp;
+	nLoad++;		
+
+	var nw = imageRepository.ship.naturalWidth;
+	var nh = imageRepository.ship.naturalHeight;
+	var sp = new SpriteImage(cw/2, ch-ch/6, nw, nh, 3, false, imageRepository.ship);
+	spArray[nLoad] = sp;
+	nLoad++;	
+/*
+	var nw = imageRepository.bullet.naturalWidth;
+	var nh = imageRepository.bullet.naturalHeight;
+	var sp = new SpriteImage(200, 200, nw, nh, 3, false, imageRepository.bullet);
+	spArray[nLoad] = sp;
+	nLoad++;*/
 
 	window.addEventListener("keydown", keydownHandler);
-	window.addEventListener("keyup", keyupHandler);	
-
-	function shipsLoader(ev) {
-		var img = ev.target;
-		var nw = img.naturalWidth;
-		var nh = img.naturalHeight;
-
-		var cw = ctx.canvas.width;
-		var ch = ctx.canvas.height;
-
-		var sp = new SpriteImage(cw/2, ch-ch/6, nw, nh, 3, false, img);
-		spArray[nLoad] = sp;
-		nLoad++;		
-
-		var ev2 = new Event("initend");
-		ev2.spArray = spArray;
-		ctx.canvas.dispatchEvent(ev2);
-	}
-
-	function bgLoader(ev)
-	{
-		var img = ev.target;
-		var nw = img.naturalWidth;
-		var nh = img.naturalHeight;
-
-		var cw = ctx.canvas.width;
-		var ch = ctx.canvas.height;
-
-		var sp = new SpriteImage(0, 0, nw, nh, 10, false, img);
-		spArray[nLoad] = sp;
-		nLoad++;		
-
-		var ev2 = new Event("initend");
-		ev2.spArray = spArray;
-		ctx.canvas.dispatchEvent(ev2);
-		
-	}
-/*
-	function keydownHandler(ev) {
-		var sp = spArray[1];
-		console.log(ev.keyCode);
-
-		spArray[1].clear(ctx)
-		switch (ev.keyCode) {
-           	case 37:
-           		if (sp.x - sp.speed >= 0)
-	          		sp.moveLeft();
-	          	break;
-          	case 39:
-          		
-	          		sp.moveRight();
-	          	break;
-          	case 38:
-          		if (sp.y - sp.speed >= 0)
-	          		sp.moveUp();
-	          	break;
-          	case 40:
-          		if (sp.y + sp.speed +sp.height < ctx.canvas.height)
-	          		sp.moveDown();
-	          	break;
-          	}
-        var cw = ctx.canvas.width;
-		var ch = ctx.canvas.height;
-
-	}
-*/
+	window.addEventListener("keyup", keyupHandler);		
 
 	function keydownHandler(ev) {	
-		if (ev.keyCode == 37) 
+		if (ev.keyCode == 37 || ev.keyCode == 65) 
 			LEFT = true;
-		else if (ev.keyCode == 39) 
+		else if (ev.keyCode == 39 || ev.keyCode == 68) 
 			RIGHT = true;
-		else if (ev.keyCode == 38) 
+		else if (ev.keyCode == 38 || ev.keyCode == 87) 
 			UP = true;
-		else if (ev.keyCode == 40) 
+		else if (ev.keyCode == 40 || ev.keyCode == 83) 
 			DOWN = true;
-
+		else if (ev.keyCode == 32)
+			shoot(ctx, spArray, bulletsArray);
 	}
 
 	function keyupHandler(ev) {
-		if (ev.keyCode == 37) 
+		if (ev.keyCode == 37 || ev.keyCode == 65) 
 			LEFT = false;
-		else if (ev.keyCode == 39) 
+		else if (ev.keyCode == 39 || ev.keyCode == 68) 
 			RIGHT = false;
-		else if (ev.keyCode == 38) 
+		else if (ev.keyCode == 38 || ev.keyCode == 87) 
 			UP = false;
-		else if (ev.keyCode == 40) 
+		else if (ev.keyCode == 40 || ev.keyCode == 83) 
 			DOWN = false;
-	}	
-
+	}
+	startAnim(ctx, spArray, bulletsArray);	
 }
 
 //iniciar animação
-function startAnim(ctx, spArray)
+function startAnim(ctx, spArray, bulletsArray)
 {
 	draw(ctx, spArray);
-	animLoop(ctx, spArray);	
+	animLoop(ctx, spArray, bulletsArray);	
 }
 
 
@@ -182,45 +147,40 @@ function clear(ctx, spArray)
 	}
 }
 
+// iniciar pool
+function poolBullets(bullet) {
+	var size = SIZE_POOL;
+	var pool = [];
+
+	for (let i=0; i < size; i++) {
+		pool[i] = bullet;
+	}
+
+	for (let i=0; i < size; i++) {
+		console.log(pool[i]);
+	}
+}
+
 
 //-------------------------------------------------------------
 //--- controlo da animação: coração da aplicação!!!
 //-------------------------------------------------------------
 var auxDebug = 0;  //eliminar
-function animLoop(ctx, spArray)
+function animLoop(ctx, spArray, bulletsArray)
 {	
 	var al = function(time)
 	{
-		animLoop(ctx, spArray);
+		animLoop(ctx, spArray, bulletsArray);
 	}
 	var reqID = window.requestAnimationFrame(al);
 
-	// move nave
-	moveShip(ctx, spArray[1]);
+	render(ctx, spArray, bulletsArray, reqID);
 
-	// verifica colisao
-	//VerifyCollision(ctx, spArray);
 
-	render(ctx, spArray, reqID);
-}
-
-function moveShip(ctx, sp) {
-
-	if (LEFT)
-		if (sp.getX()  >= 0)
-	     	sp.moveLeft();
-    if (RIGHT)
-    	if (sp.x + sp.width < ctx.canvas.width)
-	     	sp.moveRight();	  
-    if (UP)
-    	if (sp.y >= 0)
-	     	sp.moveUp(); 
-    if (DOWN)
-    	if (sp.y + sp.height < ctx.canvas.height)
-	     	sp.moveDown();   
 
 }
 
+// colisoes
 function VerifyCollision(ctx, spArray) {
 	// percorrer figura e ve bounding box
 	for (let i=0; i<spArray.length; i++) {
@@ -230,49 +190,108 @@ function VerifyCollision(ctx, spArray) {
 				turbo(ctx, spArray);
 			}
 		}
-	}
-	
+	}	
 }
 
 
 //resedenho, actualizações, ...
-function render(ctx, spArray, reqID, dt)
+function render(ctx, spArray, bulletsArray, reqID, dt)
 {
 	var cw = ctx.canvas.width;
 	var ch = ctx.canvas.height;
 
+	// move nave
+	moveShip(ctx, spArray);
+	// gere balas
+	moveBullets(ctx, bulletsArray);
+
 	//apagar canvas
 	ctx.clearRect(0, 0, cw, ch);
-
 	//move background
 	backgroundMoving(ctx, spArray);
-
-
+	// verifica colisao
+	//VerifyCollision(ctx, spArray);
 	draw(ctx, spArray);
+
+	drawBullets(ctx, bulletsArray);
 }
 
 function backgroundMoving(ctx, spArray) {
+	var sp = spArray[0];
 
-	spArray[0].drawBackground(ctx);
-	spArray[0].y += 1;
+	sp.drawBackground(ctx);
+	sp.y += 1;
 
-	if (spArray[0].y >= ctx.canvas.height)
-		spArray[0].y = 0;
+	if (sp.y >= ctx.canvas.height)
+		sp.y = 0;
 	
-
 }
 
+function moveShip(ctx, spArray) {
+	var sp = spArray[1];
 
-//-------------------------------------------------------------
-//--- interacção com o rato
-//-------------------------------------------------------------
-function canvasClickHandler(ev, ctx, spArray)
-{
-	//spArray[1].clickedBoundingBox(ev, ctx);
-	if (spArray[0].clickedBoundingBox(ev, ctx))
-	{
-		spArray[0].reset(ev, ctx);		
-		animLoop(ctx, spArray);
+	if (LEFT) {
+		//sp.x -= sp.speed;
+		if (sp.getX()  >= 0)
+	     	sp.moveLeft();
+	}
+    if (RIGHT) {
+    	//sp.x += sp.speed;
+    	if (sp.x + sp.width < ctx.canvas.width)
+	     	sp.moveRight();	  
+    }
+    if (UP) {
+    	//sp.y -= sp.speed;
+    	if (sp.y >= 0)
+	     	sp.moveUp(); 
+    }
+    if (DOWN) {
+    	//sp.y += sp.speed;
+    	if (sp.y + sp.height < ctx.canvas.height)
+	     	sp.moveDown();   
+    }
+}
+
+function moveBullets(ctx, bulletsArray) {
+	var pool = bulletsArray;
+	for (var i = 0; i < pool.length; i++) {
+		if (pool[i].y >= 0) {
+			pool[i].y -= pool[i].speed;
+		}
+		else {
+			// retirar do array
+			pool.splice(i, 1);
+			countBullets--;
+		}
 	}
 }
+
+function shoot(ctx, spArray, bulletsArray) {
+	var ship = spArray[1];
+	var size = SIZE_POOL;
+
+
+	var getShipX = spArray[1].x;
+	var getShipY = spArray[1].y;
+	var getShipWidth = spArray[1].width;
+	var getShipHeigth = spArray[1].height;
+
+	if (countBullets != size) {
+		var sp = new SpriteImage(getShipX+getShipWidth/2, getShipY, imageRepository.bullet.naturalWidth, imageRepository.bullet.naturalHeight, 3, false, imageRepository.bullet);
+		bulletsArray.push(sp);
+		countBullets++;
+	}
+}
+
+function drawBullets(ctx, bulletsArray)
+{
+	var dim = bulletsArray.length;
+
+	if (dim != 0) {
+		for (let i = 0; i < dim; i++) {
+			bulletsArray[i].draw(ctx);
+		}
+	}
+}
+
 
