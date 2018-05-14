5347a9909052ced9a305f0e8af0b2d142cef8d8a
dimmaski <diogo.fernandes.12@hotmail.com>
2018-05-14 23:54:11 +0100

    f

--- play/SpriteImage.js
+++ play/SpriteImage.js
@@ -10,6 +10,18 @@ class Sprite
 		this.img = img;
 		this.alive = alive;
 		this.name = "none";
+
+		this.imageData = this.getImageData(img);
+	}
+
+	getImageData(img){
+			var canvasnova = document.createElement("canvas");
+			canvasnova.width = this.width;
+			canvasnova.height = this.height;
+
+			var ctx = canvasnova.getContext("2d");
+			ctx.drawImage(img, 0, 0, this.width, this.height);
+			return ctx.getImageData(0, 0, this.width, this.height);
 	}
 
 	draw(ctx)
@@ -23,7 +35,18 @@ class Sprite
 		ctx.clearRect(this.x, this.y, this.width, this.height);
 	}
 
-	verifyIntersect(fig) {
+	verifyIntersect(outro) {
+	    if (this.verificaColisao(outro)) {
+	        if (this.verificaColisaoPixelPorPixel(outro))
+	            return true;
+	        else
+	        	return false;
+	    }
+	    else
+	        return false;
+	}
+
+	verificaColisao(fig) {
 		if ((fig.x + fig.width) < this.x ||
 			fig.x > (this.x + this.width) ||
 			(fig.y + fig.height) < this.y ||
@@ -34,16 +57,32 @@ class Sprite
 			return true;
 	}
 
-	verify_inside(mx, my) {
-		console.log(this.img);
-		if (mx >= this.x &&
-			mx <= this.x + this.width &&
-			my >= this.y &&
-			my <= this.y + this.height)
-			return true;
-		else
-			return false;
+	verificaColisaoPixelPorPixel(outro) {
+	    let x_left = Math.floor(Math.max(outro.x, this.x));
+	    let x_right = Math.floor(Math.min(outro.x + outro.width, this.x + this.width));
+	    let y_top = Math.floor(Math.max(outro.y, this.y));
+	    let y_bottom = Math.floor(Math.min(outro.y + outro.height, this.y + this.height));
 
+	    for (let y = y_top; y < y_bottom; y++) {
+	        for (let x = x_left; x < x_right; x++) {
+	            let x_0 = Math.round(x - outro.x);
+	            let y_0 = Math.round(y - outro.y);
+	            let n_pix = y_0 * outro.width + x_0; //n pixel to check
+	            let pix_op = outro.imageData.data[4 * n_pix + 3]; //opacity (R G B A)
+
+	            let element_x_0 = Math.round(x - this.x);
+	            let element_y_0 = Math.round(y - this.y);
+	            let element_n_pix = element_y_0 * this.width + element_x_0; //n pixel to check
+	            let element_pix_op = this.imageData.data[4 * element_n_pix + 3]; //opacity (R G B A)
+
+	            if (pix_op == 255 && element_pix_op == 255) {
+	                /*Debug*/
+	                //console.log("colisao no pixel");
+	                return true;
+	            }
+	        }
+	    }
+	    return false;
 	}
 	getType() {
 		return "none";
